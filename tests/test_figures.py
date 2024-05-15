e47f5b78a069bf52bc3906d7144897f9b0b189cd
Boris Gorelik <boris@gorelik.net>
2024-05-15 18:12:06 +0300

    Added datafiles

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -10,38 +10,69 @@ def figure_dir(book_dir):
 
 
 @pytest.fixture
-def image_credit_file(project_dir):
+def data_dir(book_dir):
+    return os.path.join(book_dir, "data")
+
+
+@pytest.fixture
+def credit_file(project_dir):
     return os.path.join(project_dir, "data_credits.json")
 
 
-def test_every_figure_has_a_credit(figure_dir, image_credit_file):
+def test_every_figure_has_a_credit(figure_dir, credit_file):
     figure_extensions = [".png", ".jpg", ".jpeg", ".svg"]
     image_files = {
         f for f in os.listdir(figure_dir) if os.path.splitext(f)[1] in figure_extensions
     }
-    image_credits = json.load(open(image_credit_file))
+    image_credits = json.load(open(credit_file))["figures"]
     covered_image_files = {v["filename"] for v in image_credits.values()}
     missing_credits = set(image_files) - covered_image_files
     assert not missing_credits, f"Missing credits for {missing_credits}"
 
 
-def test_every_credit_has_a_figure(figure_dir, image_credit_file):
+def test_every_datafile_has_a_credit(data_dir, credit_file):
+    data_extensions = [".txt", ".csv", ".json", ".gz"]
+    data_files = {
+        f for f in os.listdir(data_dir) if os.path.splitext(f)[1] in data_extensions
+    }
+    data_credits = json.load(open(credit_file))["data"]
+    covered_data_files = {v["filename"] for v in data_credits.values()}
+    missing_credits = set(data_files) - covered_data_files
+    assert not missing_credits, f"Missing credits for {missing_credits}"
+
+
+def test_every_credit_has_a_figure(figure_dir, credit_file):
     figure_extensions = [".png", ".jpg", ".jpeg", ".svg"]
     image_files = {
         f for f in os.listdir(figure_dir) if os.path.splitext(f)[1] in figure_extensions
     }
-    image_credits = json.load(open(image_credit_file))
+    image_credits = json.load(open(credit_file))["figures"]
     covered_image_files = {v["filename"] for v in image_credits.values()}
     extra_credits = covered_image_files - image_files
     assert not extra_credits, f"Extra credits for {extra_credits}"
 
 
-def test_credit_has_correct_format(image_credit_file):
-    required_fields = ["filename", "author", "description", "source", "url", "license"]
-    image_credits = json.load(open(image_credit_file))
-    bad_credits = {
-        k: v
-        for k, v in image_credits.items()
-        if not all(f in v for f in required_fields)
+def test_every_credit_has_a_datafile(data_dir, credit_file):
+    data_extensions = [".txt", ".csv", ".json", ".gz"]
+    data_files = {
+        f for f in os.listdir(data_dir) if os.path.splitext(f)[1] in data_extensions
     }
-    assert not bad_credits, f"Bad credits for {bad_credits}"
+    data_credits = json.load(open(credit_file))["data"]
+    covered_data_files = {v["filename"] for v in data_credits.values()}
+    extra_credits = covered_data_files - data_files
+    assert not extra_credits, f"Extra credits for {extra_credits}"
+
+
+def test_credit_has_correct_format(credit_file):
+    required_fields = ["filename", "author", "description", "source", "url", "license"]
+    credits = json.load(open(credit_file))
+    bad_credits = dict()
+    for scope, scope_credits in credits.items():
+        curr_bad = {
+            k: v
+            for k, v in scope_credits.items()
+            if not all(f in v for f in required_fields)
+        }
+        if curr_bad:
+            bad_credits[scope] = curr_bad
+    assert not bad_credits, f"Bad credits: {bad_credits}"
