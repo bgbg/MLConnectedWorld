210412165b8f2d68e2b1d1a65e2978a7b5d03d7d
Boris Gorelik <boris@gorelik.net>
2024-05-16 10:54:39 +0300

    More modular graph loading

--- graph_utils.py
+++ graph_utils.py
@@ -9,14 +9,8 @@ from cachier import cachier
 import tempfile
 
 
-@cachier(stale_after=timedelta(days=100))
-def get_graph(dataset_name) -> Union[nx.Graph, list]:
-    """Load a graph from a data collection repository"""
-
-    dir_this = os.path.dirname(os.path.abspath(__file__))
-    dir_book = os.path.join(dir_this, "MLConnectedWorldBook")
-    dir_data = os.path.join(dir_book, "data")
-    assert os.path.exists(dir_data), f"Data directory {dir_data} not found"
+def get_graph(dataset_name: str) -> Union[nx.Graph, list]:
+    """Load a graph from a data collection repository or list available datasets."""
 
     data_names = {
         "ca-AstroPh": "ca-AstroPh.txt.gz",
@@ -24,51 +18,51 @@ def get_graph(dataset_name) -> Union[nx.Graph, list]:
         "ca-GrQc": "ca-GrQc.txt.gz",
         "ca-HepPh": "ca-HepPh.txt.gz",
         "ca-HepTh": "ca-HepTh.txt.gz",
-        # "cit-Patents": "cit-Patents.txt.gz",
     }
 
     if dataset_name == "list":
         return list(data_names.keys())
 
-    dataset_path = None
-    for extension in ["", ".csv", ".csv.gz"]:
-        curr = os.path.join(dir_data, dataset_name + extension)
-        if os.path.exists(curr):
-            dataset_path = curr
-            break
-    if dataset_path is not None:
-        # found locally
-        df = pd.read_csv(dataset_path)
-        assert "src" in df.columns, f"Column 'src' not found in {dataset_path}"
-        assert "dst" in df.columns, f"Column 'dst' not found in {dataset_path}"
-        edge_attr = [c for c in df.columns if c not in ["src", "dst"]]
-        G = nx.from_pandas_edgelist(
-            df,
-            source="src",
-            target="dst",
-            edge_attr=edge_attr,
+    try:
+        return load_graph_from_local(dataset_name)
+    except FileNotFoundError:
+        pass
+
+    if dataset_name not in data_names:
+        raise ValueError(
+            f"Dataset {dataset_name} not available. Choose from {list(data_names.keys())}."
         )
-        return G
+    return load_graph_from_web(data_names[dataset_name])
 
-    url_base = "https://snap.stanford.edu/data/"
 
-    if dataset_name.endswith(".txt.gz"):
-        url = dataset_name
-        if not url.startswith("http"):
-            url = url_base + url
-    else:
-        if dataset_name not in data_names:
-            msg = (
-                f"Dataset {dataset_name} not available. "
-                f" Choose from {list(data_names.keys())}, "
+def load_graph_from_local(dataset_name: str) -> nx.Graph:
+    """Load a graph from a local file."""
+    dir_this = os.path.dirname(os.path.abspath(__file__))
+    dir_data = os.path.join(dir_this, "MLConnectedWorldBook", "data")
+    assert os.path.exists(dir_data), f"Data directory {dir_data} not found"
+
+    for extension in ["", ".csv", ".csv.gz"]:
+        dataset_path = os.path.join(dir_data, dataset_name + extension)
+        if os.path.exists(dataset_path):
+            df = pd.read_csv(dataset_path)
+            assert "src" in df.columns, f"Column 'src' not found in {dataset_path}"
+            assert "dst" in df.columns, f"Column 'dst' not found in {dataset_path}"
+            edge_attr = [c for c in df.columns if c not in ["src", "dst"]]
+            G = nx.from_pandas_edgelist(
+                df, source="src", target="dst", edge_attr=edge_attr
             )
-            raise ValueError(msg)
-        url = url_base + data_names[dataset_name]
+            return G
+    raise FileNotFoundError("Local dataset file not found")
+
+
+@cachier(stale_after=timedelta(days=100))
+def load_graph_from_web(filename: str) -> nx.Graph:
+    """Download and load a graph from the web."""
+    url_base = "https://snap.stanford.edu/data/"
+    url = url_base + filename
     with tempfile.TemporaryDirectory() as tmpdirname:
         fn = os.path.join(tmpdirname, "graph.txt.gz")
-        # download the file
         urllib.request.urlretrieve(url, fn)
-        # load the graph
         G = nx.read_edgelist(fn, nodetype=int)
     return G
 
@@ -87,10 +81,3 @@ def get_info(G: nx.Graph):
     else:
         ret.append("Graph is not connected")
     return "\n".join(ret)
-
-
-if __name__ == "__main__":
-    n = "corporate_undirected.csv"
-    get_graph.clear_cache()
-    g = get_graph(n)
-    print(get_info(g))
